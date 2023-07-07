99cdd902357d185c711c823d49dbc0bc864d681c
dun <DundeeDevelops@gmail.com>
2023-07-07 06:26:29 -0500

    Deleted useless files , added drumpads

---
+++ src/components/DrumPad.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useRef, useEffect } from "react";
+
+const DrumPad = (props) =>{
+    const btnRef = useRef(null);
+
+
+
+        return(
+            <button className="drum-pad" onClick={() => btnRef.current.play()}>
+            <audio ref={btnRef} className="clip" id={props.btn} src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3">
+            </audio>
+            {props.btn}
+            </button> 
+        )
+        
+
+}
+
+export default DrumPad;
