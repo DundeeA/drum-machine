59d8a81cd21e1505e51b23ad74867d93fbd16c09
dun <DundeeDevelops@gmail.com>
2023-07-07 07:53:38 -0500

    Added all sound files , sound clip names

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -6,6 +6,44 @@ class DrumMachine extends React.Component {
     super();
     this.state = {
       controlKeys: ["q", "w", "e", "a", "s", "d", "z", "x", "c"],
+      audioClips: [
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+          title: "Heater 1",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+          title: "Heater 2",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+          title: "Heater 3",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+          title: "heater 4",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+          title: "Clap",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+          title: "TSk",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+          title: "Kick/hat",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+          title: "Bass",
+        },
+        {
+          link: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+          title: "Tsk 2",
+        },
+      ],
     };
 
     this.detectKeyDown = this.detectKeyDown.bind(this);
@@ -22,14 +60,19 @@ class DrumMachine extends React.Component {
   }
 
   render() {
-    const drumPads = this.state.controlKeys.map((pad) => (
-      <DrumPad key={pad + 1} btn={pad} />
+    const drumPads = this.state.controlKeys.map((pad, i) => (
+      <DrumPad
+        key={pad + 1}
+        btn={pad}
+        clip={this.state.audioClips[i].link}
+        title={this.state.audioClips[i].title}
+      />
     ));
 
     return (
       <div id="drum-machine">
-        <div id="display"></div>
         {drumPads}
+        <div id="display"></div>
       </div>
     );
   }
