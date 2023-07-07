706d7c3b6f56fdb2bbe8a8d8662a5013957bd401
dun <DundeeDevelops@gmail.com>
2023-07-07 06:28:42 -0500

    Fixed indentation

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -2,36 +2,37 @@ import React from "react";
 import DrumPad from "./DrumPad";
 
 class DrumMachine extends React.Component {
-    constructor(){
-        super();
-        this.state={
-          controlKeys: ['q','w','e','a','s','d','z','x','c']
-        }
+  constructor() {
+    super();
+    this.state = {
+      controlKeys: ["q", "w", "e", "a", "s", "d", "z", "x", "c"],
+    };
 
-        this.detectKeyDown = this.detectKeyDown.bind(this);
-    }
+    this.detectKeyDown = this.detectKeyDown.bind(this);
+  }
 
-    detectKeyDown(e) {
-      if(this.state.controlKeys.includes(e.key)){
+  detectKeyDown(e) {
+    if (this.state.controlKeys.includes(e.key)) {
       document.getElementById(e.key).play();
-      }
-    }  
-    
-   componentDidMount(){
-    document.addEventListener('keydown', this.detectKeyDown, true);
-   }
-
-    render(){
-        const drumPads = this.state.controlKeys.map(pad => <DrumPad key={pad + 1} btn={pad}/>)
-
-        return(
-         <div id="drum-machine">
-          <div id="display"></div>
-          {drumPads}
-         </div>
-        ) 
     }
-}
+  }
 
+  componentDidMount() {
+    document.addEventListener("keydown", this.detectKeyDown, true);
+  }
+
+  render() {
+    const drumPads = this.state.controlKeys.map((pad) => (
+      <DrumPad key={pad + 1} btn={pad} />
+    ));
+
+    return (
+      <div id="drum-machine">
+        <div id="display"></div>
+        {drumPads}
+      </div>
+    );
+  }
+}
 
 export default DrumMachine;
